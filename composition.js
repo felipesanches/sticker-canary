2d35f487cdfcdf6f7e869e9c162b3df506bf16cb
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2010-10-27 23:12:58 -0300

    * add icons * store image transform as a dictionary in json instead of storing an SVG transform string * render transparent image for reference * proof of concept of rotation controls (just to test live editing of transforms)

--- composition.js
+++ composition.js
@@ -24,14 +24,38 @@ function Composition(cName, dPage) {
   this.stickerLayout.height = stickerCanary.toUserUnit(this.stickerLayout.height);
 }
 
+Composition.prototype.serialize_transform = function(t, w, h){
+  var result = "translate(" + (t.x - w/2) + "," + (t.y - h/2) + ") ";
+  if (t.scale) result += "scale("+t.scale+") ";
+  result += "rotate("+t.rotate+") "+
+            "translate(" + w/2 + "," + h/2 + ")";
+  return result;
+}
+
+Composition.prototype.serialize_transform = function(t, w, h){
+  if (!t.scale) t.scale=1;
+  return "translate(" + (t.x + w/2) + "," + (t.y + h/2) + ")"+
+         "scale("+t.scale+") "+
+         "rotate("+t.rotate+") "+
+         "translate(" + (-w/2)/t.scale + "," + (-h/2)/t.scale + ") ";
+}
+
 Composition.prototype.evalCompositionFrontCode = function(code) {
   var clipId = "#composition-clip-"+this.conf.matrix.rows+"-"+this.conf.matrix.cols;
+  var width = stickerCanary.toUserUnit( this.stickerLayout.width ) * this.conf.matrix.cols;
+  var height = stickerCanary.toUserUnit( this.stickerLayout.height ) * this.conf.matrix.rows;
   var currentCompositionImage =
       "<g clip-path='url("+ clipId +")'>"+
            "<image xlink:href='"+ this.conf.img + "'"+
                  " width='100%' height='100%'"+
                  " preserveAspectRatio='xMinYMin meet'"+
-                 " transform='"+ this.conf.transform +"'/>"+
+                 " transform='"+ this.serialize_transform(this.conf.transform, width, height) +"'/>"+
+      "</g>" +
+      "<g style='opacity:0.3'>"+
+           "<image xlink:href='"+ this.conf.img + "'"+
+                 " width='100%' height='100%'"+
+                 " preserveAspectRatio='xMinYMin meet'"+
+                 " transform='"+ this.serialize_transform(this.conf.transform, width, height) +"'/>"+
       "</g>"
 
   var currentComposition = {
@@ -39,8 +63,8 @@ Composition.prototype.evalCompositionFrontCode = function(code) {
     y: stickerCanary.toUserUnit(this.conf.y),
     img: currentCompositionImage,
     label: this.conf.label,
-    width: stickerCanary.toUserUnit( this.stickerLayout.width ) * this.conf.matrix.cols,
-    height: stickerCanary.toUserUnit( this.stickerLayout.height ) * this.conf.matrix.rows,
+    width: width,
+    height: height
   };
 //  var margin = stickerCanary.toUserUnit( this.compositionLayout.margin );
   return eval(code);
@@ -124,15 +148,52 @@ Composition.prototype.generateFront = function() {
   var parser = new DOMParser();
   var dom = parser.parseFromString('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">'+svgCode+"</svg>", "text/xml");
 
-  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  const SVG_NS = "http://www.w3.org/2000/svg";
+  var group = document.createElementNS(SVG_NS, "g");
   group.setAttribute("class", "composition-front");
-  group.setAttribute("transform", "translate("+ this.conf.x +","+ this.conf.y 
-                                  +") rotate("+this.conf.rotate+")");
+  group.setAttribute("transform", "rotate("+this.conf.rotate+") translate("+ this.conf.x +","+ this.conf.y+")");
   // Copy the composition front elements to the page:
   while ( dom.documentElement.hasChildNodes() ) {
     group.appendChild( dom.documentElement.firstChild );
   };
   svg.appendChild(group);
+  
+  var self = this;
+  var img = group.getElementsByTagName("image");
+  
+  var width = stickerCanary.toUserUnit( this.stickerLayout.width ) * this.conf.matrix.cols;
+  var height = stickerCanary.toUserUnit( this.stickerLayout.height ) * this.conf.matrix.rows;
+
+  //add controls:
+  var inc_scale = document.createElementNS(SVG_NS, "rect");
+  inc_scale.setAttribute("fill", "green");
+  inc_scale.setAttribute("x", 10);
+  inc_scale.setAttribute("y", 10);
+  inc_scale.setAttribute("width", 10);
+  inc_scale.setAttribute("height", 10);
+  group.appendChild(inc_scale);
+  inc_scale.onclick= function(){
+    self.conf.transform.rotate+=1;
+    img[0].setAttribute("transform",
+        self.serialize_transform(self.conf.transform, width, height));
+    img[1].setAttribute("transform",
+        self.serialize_transform(self.conf.transform, width, height));
+  };
+
+  var dec_scale = document.createElementNS(SVG_NS, "rect");
+  dec_scale.setAttribute("fill", "red");
+  dec_scale.setAttribute("x", 10);
+  dec_scale.setAttribute("y", 20);
+  dec_scale.setAttribute("width", 10);
+  dec_scale.setAttribute("height", 10);
+  group.appendChild(dec_scale);
+  dec_scale.onclick= function(){
+    self.conf.transform.rotate-=1;
+    img[0].setAttribute("transform",
+        self.serialize_transform(self.conf.transform, width, height));
+    img[1].setAttribute("transform",
+        self.serialize_transform(self.conf.transform, width, height));
+  }
 }
 
 Composition.prototype.generateBack = function() {
